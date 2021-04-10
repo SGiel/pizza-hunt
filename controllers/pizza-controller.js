80f48d09e9163fac743737e18d92daa980b573e7
Stephanie Giel <stephaniegiel@gmail.com>
2021-04-10 11:56:40 -0700

    created user-model

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,87 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  // get all pizzas
+  // will serve as the callback function for the GET /api/pizzas route
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id -- instead of accessing entire req, destructured params out of it
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => {
+        // If no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // createPizza
+  // we destructured the body out of the Express.js req object because we 
+  // don't need to interface with any of the other data it provides
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update pizza by id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update pizza by id
+  updatePizza({ params, body }, res) {
+    // if we don't set that third parameter, { new: true }, it will return the original document. 
+    // By setting the parameter to true, we're instructing Mongoose to return the new version of the document.
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+}
+
+module.exports = pizzaController;
+
+// In MongoDB, the methods for adding data to a collection are .insertOne() or .insertMany(). 
+// But in Mongoose, we use the .create() method, which will actually handle either one or multiple inserts!
+// There are also Mongoose and MongoDB methods called .updateOne() and .updateMany(), which update documents without returning them.
